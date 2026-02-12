85d31154c7c541ba904681837146bfa9d9881f2a
jachymsmid <jerejere986@gmail.com>
2026-02-12 10:24:47 +0100

    corrected index in for loop

--- advection_1d/src/JacobiP.hpp
+++ advection_1d/src/JacobiP.hpp
@@ -1,29 +1,30 @@
 #pragma once
 
 template <class RealType>
-RealType JacobiP_point(int n, const RealType a, const RealType b, RealType& x)
+RealType JacobiP_point(int N, const RealType a, const RealType b, RealType& x)
 {
-  RealType p_x_n, a_n, b_n, c_n, d_n;
+  RealType p_x_n, a_n, b_n, c_n, d_n, n_r;
   RealType p_x_0 = 1.f;
   RealType p_x_1 = (a-b+(a+b+2)*x)/2;
-  if (n==0)
+  if (N==0)
   {
     return p_x_0;
   }
-  else if (n==1)
+  else if (N==1)
   {
     return p_x_1;
   }
   else
   {
-    for (int i = 0; i < n; i++)
+    for (int n = 2; n < N+1; n++)
     {
-      a_n = 2*(n+1)*(n+a+b+1)*(2*n+a+b);
-      b_n = (2*n+a+b+1)*(a*a-b*b);
-      c_n = (2*n+a+b)*(2*n+a+b+1)*(2*n+a+b+2);
-      d_n = 2*(n+a)*(n+b)*(2*n+a+b+2);
+      n_r = (RealType) n;
+      a_n = 2*(n_r+1)*(n_r+a+b+1)*(n_r*2+a+b);
+      b_n = (n_r*2+a+b+1)*(a*a-b*b);
+      c_n = (2*n_r+a+b)*(2*n_r+a+b+1)*(2*n_r+a+b+2);
+      d_n = 2*(n_r+a)*(n_r+b)*(2*n_r+a+b+2);
 
-      p_x_n = ((b_n+c_n*x)*P_x_1-d_n*P_x_0)/a_n;
+      p_x_n = ((b_n+c_n*x)*p_x_1-d_n*p_x_0)/a_n;
 
       p_x_0 = p_x_1;
       p_x_1 = p_x_n;
