c1cee1c38babfd25484fdb39312d26e463cd66f1
Jáchym Šmíd <jerejere986@gmail.com>
2026-03-16 10:39:48 +0100

    unstructured mesh object

--- advection_1d/src/Mesh.hpp
+++ advection_1d/src/Mesh.hpp
@@ -1,1 +1,26 @@
+#pragma once
 
+template
+<
+  class RealType,
+  class VectorType,
+  class MatrixType,
+  int number_of_elements,
+  VectorType degree_of_freedom
+>
+class Mesh
+{
+public:
+  // constructor
+  Mesh()
+  {
+
+  }
+
+private:
+  VectorType _vertex_cord;
+  MatrixType _EToV;
+  VectorType _x_cord;
+  int _number_elements;
+  VectorType _dof;
+};
