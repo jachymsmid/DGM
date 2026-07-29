deb7b39dd759321671cd725e9708e3e6b6edcddf
jachymsmid <jachymsmid@users.noreply.github.com>
2026-07-29 15:13:29 +0000

    deploy: 3e92af0918482c9177d487f78380c47e4862f9b7

---
+++ search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mathematical_20background_0',['Mathematical background',['../PadeLegendre_8hpp.html#autotoc_md0',1,'Mathematical background'],['../PadeLegendre_8hpp.html#autotoc_md1',1,'Mathematical background']]]
+];
